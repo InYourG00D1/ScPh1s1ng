a05c7381f78274f5d8d8d550f45863c2ab214232
Ragil}{P <44290386+InYourG00D1@users.noreply.github.com>
2020-06-05 20:28:34 +0700

    Add files via upload

---
+++ 6.sh
@@ -0,0 +1,398 @@
+#!/system/xbin/bash
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m' 
+yellow='\033[33;1m'
+
+echo $red "I"
+sleep 1
+clear
+echo $blue "In";
+sleep 1
+clear
+echo $green "InY";
+sleep 1
+clear
+echo $red "InYo";
+sleep 
+clear
+echo $blue "InYou";
+sleep 1
+clear
+echo $green "InYour";
+sleep 1
+clear
+echo $red "InYourG0";
+sleep 1
+clear
+echo $blue "InYourG00";
+sleep 1
+clear
+echo $green "InYourG00D";
+sleep 1
+clear
+echo ""
+
+echo "\033[1;31m]
+                /^...^
+               /<_* *_>   
+              /\/ \_/ 
+      ___.-._/\/
+     /=*_*-*-*/!!!
+    |-{-_-_-} !!!
+    (-{-_-_-} !!
+     \{_-_-_} ! 
+      }-_-_-} [?] For Tutor Contact IG : @ragil_iygd77
+      {-_|-_} [?] Domain : .com .org .co.id 
+      {-_|_-} [!] Cpanel is also available
+      {_-|-_} [!] For the price you can talk about it
+      {_-|-_} 
++=====%%@ @%%=============================+
++ coded by : InYourG00D                   +
++ Team : LNX#.CREW S.T.C BUFT             +
++ GitHub : https://github.com/InYourG00D1 +
++=========================================+
+                              Version 3.1.0
+  ^...^                       ^...^
+ <_* *_> Script Phising Game <_* *_> 
+   \_/                         \_/   ";
+echo $blue "-=[Menu]=-"
+echo $green "[================================[>";
+echo "-=:FreeFire:=-"|lolcat
+echo $red "[================================[>";
+echo "1.bundel gg"|lolcat
+echo $yellow "[================================[>";
+echo "2.Viral"|lolcat
+echo $green "[================================[>";
+echo "3.DiamondGratis"|lolcat
+echo $red "[================================[>";
+echo "-=:MobileLegends:=-"|lolcat
+echo $yellow "[================================[>";
+echo "4.Viral"|lolcat
+echo $green "[================================[>";
+echo "-=:CoC:=-"|lolcat
+echo $cyan "[================================[>";
+echo "5.Versi Turnament"|lolcat
+echo $yellow "[================================[>";
+echo "6.Viral"|lolcat
+echo $purple "[================================[>";
+echo "-=:Pubg:=-"|lolcat
+echo $red "[================================[>";
+echo "7.TrueLogin"|lolcat
+echo $yellow "[================================[>";
+echo "8.UC"|lolcat
+echo $green "[================================[>";
+echo "9.Viral"|lolcat
+echo $blue "[================================[>";
+echo "10.TrueLogin"|lolcat
+echo $red "[================================[>";
+echo "11.Viral(2)"|lolcat
+echo $cyan "[================================[>";
+echo "-=:PB Evolved:=-"|lolcat
+echo $green "[================================[>";
+echo "12.TrueLogin"|lolcat
+echo $yellow "[================================[>";
+echo "-=:FaceBook:=-"|lolcat
+echo $green "[================================[>";
+echo "13.WarnaWarni"|lolcat
+echo $blue "[================================[>"
+echo "-=:AoV:=-"|lolcat
+echo $purple "[================================[>"
+echo "14.Viral"|lolcat
+echo $red "[================================[>"
+echo "-=:Instagram:=-"|lolcat
+echo $green "[================================[>"
+echo "15.Generator Liker"|lolcat
+echo $yellow "[================================[>"
+echo "16.Auto Like"|lolcat
+echo $green "[================================[>"
+echo "00.Out"|lolcat
+echo $red
+read -p "[>>" gmt
+
+if [ $gmt = 1 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f bundle.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name bundle.zip";
+fi
+
+if [ $gmt = 2 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f ff.viral.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name ff.viral.zip";
+fi
+
+if [ $gmt = 3 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f DG.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name DG.zip";
+fi
+
+if [ $gmt = 4 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f Ml.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name Ml.zip"
+fi
+
+if [ $gmt = 5 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f Tour.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name Tour.zip";
+fi
+
+if [ $gmt = 6 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f coc.viral.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name coc.viral.zip";
+fi
+
+if [ $gmt = 7 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f truelogin.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name truelogin.zip";
+fi
+
+if [ $gmt = 8 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f UC.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name UC.zip";
+fi
+
+if [ $gmt = 9 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f viral2.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name viral2.zip"
+fi
+
+if [ $gmt = 10 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f viral3.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name viral3.zip";
+fi
+
+if [ $gmt = 11 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f viral4.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name viral4.zip";
+fi
+
+if [ $gmt = 12 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f evolved.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $blue "[!] File/Folder Name evolved.zip";
+fi
+
+if [ $gmt = 13 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f pelangi.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name pelangi.zip";
+fi
+
+if [ $gmt = 14 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f aov.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name aov.zip";
+fi
+
+if [ $gmt = 15 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f gl.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name gl.zip";
+fi
+
+if [ $gmt = 16 ]
+then 
+clear
+echo "Wait Bro.";
+sleep 1
+clear 
+echo "Wait Bro..";
+sleep 
+clear 
+echo "Wait Bro...";
+cd menu
+mv -f Autolike.zip /sdcard
+echo $red "DoneBosQ";
+echo $green "Silahkan Cek Di Folder/file";
+echo $red "[!] File/Folder Name Autolike.zip";
+fi
+
+if [ $gmt = 00 ]
+then 
+clear
+echo "Thanks Using Tool Me";
+echo "Bye";
+exit
+fi
